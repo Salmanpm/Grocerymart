b1735bc1243718efe71a0043bc175132e1e69e45
Salman <sallusalmanmadoll916@gmail.com>
2025-07-10 02:15:21 +0530

    Some new changes in backend server.js

--- server/server.js
+++ server/server.js
@@ -19,26 +19,15 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://grocerymart-eta.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', 'https://grocerymart-eta.vercel.app']
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({origin: allowedOrigins, credentials: true}))
+app.use(cors({origin: allowedOrigins, credentials: true}))
 
 
 
